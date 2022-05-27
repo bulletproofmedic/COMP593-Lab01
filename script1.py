8cb7bd555564f8ed0339915385e8a6ab084e9f83
bulletproofmedic <98325444+bulletproofmedic@users.noreply.github.com>
2022-05-27 16:09:30 -0400

    Changed stuff

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #Print from Zen of Python
 print('Sparse is better than dense.')
-print('Readability counts.')
--- script2.py
+++ script2.py
@@ -1,3 +1,5 @@
 #This is good programming advice.
 print('Simple is better than complex.')
 print('Complex is better than complicated.')
+print('This is a line.')
+print('This is a second line.')
